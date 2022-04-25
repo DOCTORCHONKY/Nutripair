e87bba6c64aa2043e1810cb3f5b9aedb06932afe
jasonnguyen0310 <jasonnguyen0310@gmail.com>
2022-04-25 16:12:20 -0700

    Janky nutrition script

--- Scripts/nutrition_jason.py
+++ Scripts/nutrition_jason.py
@@ -31,16 +31,16 @@ def read_excel(fileName):
 	for i, row in excel_data.iterrows():
 		# if menu item cell is not null -> add it as a menu item
 		if not pd.isnull(row["MENU ITEM"]):
-			food_data[row["MENU ITEM"]] = defaultdict(str)
+			food_data[row["MENU ITEM"]] = defaultdict(tuple)
 			currMenuItem = row["MENU ITEM"]
 		# if menu item cell is null, we are working with previous menu item
 		else:
 			# food_data[currentMenuItem][ingredient] = (measurement,raw/not raw)
 			# RAW = 1, NOT RAW = 0
 			if (row["RAW"] == 'x'):				
-				food_data[currMenuItem][row["INGREDIENTS"]] = (row["MEASUREMENTS"],1)
+				food_data[currMenuItem][row["INGREDIENTS"]] = (float(row["MEASUREMENTS"]),1)
 			else:
-				food_data[currMenuItem][row["INGREDIENTS"]] = (row["MEASUREMENTS"],0)
+				food_data[currMenuItem][row["INGREDIENTS"]] = (float(row["MEASUREMENTS"]),0)
 
 	# return dictionary with all food ingredient data
 	return food_data
@@ -74,7 +74,7 @@ Input: (string: foodItem)
 Output: (dict[nutrition]: nutritionInformation)
 '''
 def query_ingredient(ingredient):
-	nutrition_info = {}
+	nutrition_info = defaultdict(float)
 
 	index_name = 'usdafoods'
 	client = connect()
@@ -95,28 +95,45 @@ def query_ingredient(ingredient):
 
 	print('\nSearch results:')
 	print(response)
+	
+	
+	try:
+		print("Food: ", 0, "Category: ", response["hits"]["hits"][0]["_source"]["Food_Description"])
+		nutrition_info["CALORIES"] = float(response["hits"]["hits"][0]["_source"]["Energy_kcal"])
+		print("CALORIES: ", response["hits"]["hits"][0]["_source"]["Energy_kcal"])
+		nutrition_info["TOTAL FAT (g)"] =  float(response["hits"]["hits"][0]["_source"]["Total_Fat_g"])
+		print("TOTAL FAT (g): ", response["hits"]["hits"][0]["_source"]["Total_Fat_g"])
+		nutrition_info["SATURATED FAT (g)"] = float(response["hits"]["hits"][0]["_source"]["Saturated_Fat_g"])
+		print("SATURATED FAT (g): ", response["hits"]["hits"][0]["_source"]["Saturated_Fat_g"])
+		nutrition_info["TRANS FAT (g)"] = float(response["hits"]["hits"][0]["_source"]["Monounsaturated_Fat_g"]) # MISSING
+		print("TRANS FAT (g): ", response["hits"]["hits"][0]["_source"]["Monounsaturated_Fat_g"])
+		nutrition_info["CHLOESTEROL (mg)"] = float(response["hits"]["hits"][0]["_source"]["Cholesterol_mg"])
+		print("CHLOESTEROL (mg): ", response["hits"]["hits"][0]["_source"]["Cholesterol_mg"])
+		nutrition_info["SODIUM (mg)"] = float(response["hits"]["hits"][0]["_source"]["Sodium_mg"])
+		print("SODIUM (mg): ", response["hits"]["hits"][0]["_source"]["Sodium_mg"])
+		nutrition_info["TOTAL CARBOHYDRATE (g)"] = float(response["hits"]["hits"][0]["_source"]["Carbohydrate_g"])
+		print("TOTAL CARBOHYDRATE (g): ", response["hits"]["hits"][0]["_source"]["Carbohydrate_g"])
+		nutrition_info["DIETARY FIBER (g)"] = float(response["hits"]["hits"][0]["_source"]["Total_Dietary_Fiber_g"])
+		print("DIETARY FIBER (g): ", response["hits"]["hits"][0]["_source"]["Total_Dietary_Fiber_g"])
+		nutrition_info["TOTAL SUGARS (g)"] = float(response["hits"]["hits"][0]["_source"]["Total_Sugars_g"])
+		print("TOTAL SUGARS (g): ", response["hits"]["hits"][0]["_source"]["Total_Sugars_g"])
+		nutrition_info["ADDED SUGARS (g)"] = 0
+		print("ADDED SUGARS (g): ", "0") # MISSING
+		nutrition_info["PROTEIN (g)"] = float(response["hits"]["hits"][0]["_source"]["Protein_g"])
+		print("PROTEIN (g): ", response["hits"]["hits"][0]["_source"]["Protein_g"])
+		nutrition_info["VITAMIN D (mcg)"] = float(response["hits"]["hits"][0]["_source"]["Vitamin_D_mcg"]) # MCG not MG
+		print("VITAMIN D (mcg): ", response["hits"]["hits"][0]["_source"]["Vitamin_D_mcg"]) 
+		nutrition_info["CALCIUM (mg)"] = float(response["hits"]["hits"][0]["_source"]["Calcium_mg"])
+		print("CALCIUM (mg): ", response["hits"]["hits"][0]["_source"]["Calcium_mg"])
+		nutrition_info["IRON (mg)"] = float(response["hits"]["hits"][0]["_source"]["Iron_mg"])
+		print("IRON (mg): ", response["hits"]["hits"][0]["_source"]["Iron_mg"])
+		nutrition_info["POTASSIUM (mg)"] = float(response["hits"]["hits"][0]["_source"]["Potassium_mg"])
+		print("POTASSIUM (mg): ", response["hits"]["hits"][0]["_source"]["Potassium_mg"])
+	except:
+		print("Can't find ingredient")
+	
 
-	for i in range(response["hits"]["total"]["value"] if response["hits"]["total"]["value"] <= 10 else 10):
-		try:
-			print("Food: ", i, "Category: ", response["hits"]["hits"][i]["_source"]["Food_Description"])
-			print("CALORIES: ", response["hits"]["hits"][i]["_source"]["Energy_kcal"])
-			print("TOTAL FAT (g): ", response["hits"]["hits"][i]["_source"]["Total_Fat_g"])
-			print("SATURATED FAT (g): ", response["hits"]["hits"][i]["_source"]["Saturated_Fat_g"])
-			print("TRANS FAT (g): ", response["hits"]["hits"][i]["_source"]["Monounsaturated_Fat_g"]) # MISSING
-			print("CHLOESTEROL (mg): ", response["hits"]["hits"][i]["_source"]["Cholesterol_mg"])
-			print("SODIUM (mg): ", response["hits"]["hits"][i]["_source"]["Sodium_mg"])
-			print("TOTAL CARBOHYDRATE (g): ", response["hits"]["hits"][i]["_source"]["Carbohydrate_g"])
-			print("DIETARY FIBER (g): ", response["hits"]["hits"][i]["_source"]["Total_Dietary_Fiber_g"])
-			print("TOTAL SUGARS (g): ", response["hits"]["hits"][i]["_source"]["Total_Sugars_g"])
-			print("ADDED SUGARS (g): ", "0") # MISSING
-			print("PROTEIN (g): ", response["hits"]["hits"][i]["_source"]["Protein_g"])
-			print("VITAMIN D (mcg): ", response["hits"]["hits"][i]["_source"]["Vitamin_D_mcg"]) # MCG not MG
-			print("CALCIUM (mg): ", response["hits"]["hits"][i]["_source"]["Calcium_mg"])
-			print("IRON (mg): ", response["hits"]["hits"][i]["_source"]["Iron_mg"])
-			print("POTASSIUM (mg): ", response["hits"]["hits"][i]["_source"]["Potassium_mg"])
-		except:
-			break
-
+	return nutrition_info
 
 ''' 
 calculate_nutrition()
@@ -124,7 +141,36 @@ Input: (dict: food_data)
 Output: (dict: nutrition_labels)
 '''
 def calculate_nutrition(food_data):
-	pass
+	nutrition_labels = {}
+	for food, ingredients in food_data.items():
+		print("Food Item: ", food)
+		for ingredient, details in ingredients.items():
+			print("Ingredient: ", ingredient)
+			measurement = details[0]
+			print("Measurement: ", measurement)
+			raw = details[1]
+			print("Raw: ", raw)
+
+
+			'''
+			if (raw):
+				nutrition_info = query_ingredient("RAW" + ingredient)
+			else:
+				nutrition_info = query_ingredient(ingredient)
+			'''
+
+			nutrition_info = query_ingredient(ingredient)
+			
+			nutrition_labels[food] = defaultdict(float)
+
+			for label, value in nutrition_info.items():
+				nutrition_labels[food][label] += float(value) * measurement
+				#nutrition_labels[food][label] += value * 2
+		
+	
+	print(nutrition_labels)
+	return nutrition_labels
+			
 
 
 
@@ -137,6 +183,6 @@ if __name__ == "__main__":
 	fileName = "Jan's Health Bar (SAMPLE MENU).xlsx"
 	# Read food item data
 	food_data = read_excel(fileName)
-	# Query milk
-	query_ingredient("milk")
+	# Calculate nutrition labels for food_data
+	nutrition_labels = calculate_nutrition(food_data)
 
